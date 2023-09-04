705f0b1bbea0f66892de508da9640c9ddd234466
sherwin wang <sherwinnw22@gmail.com>
2023-09-03 18:30:30 -0700

    Homework submitted

---
+++ WcfProject1/Cryption.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace WcfProject1
+{
+    public sealed class Cryption
+    {
+        byte[] seed = ASCIIEncoding.ASCII.GetBytes("cse44598");
+        public string encrypt (string plainString)
+        {
+            if (String.IsNullOrEmpty(plainString))
+            {
+                throw new ArgumentNullException("The input string for encryption cannot be empty or null!");
+            }
+            SymmetricAlgorithm saProvider = DES.Create();
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, saProvider.CreateEncryptor(seed,seed), CryptoStreamMode.Write);
+            StreamWriter sWriter = new StreamWriter(cStream);
+            sWriter.Write(plainString);
+            sWriter.Flush();
+            cStream.FlushFinalBlock();
+            return Convert.ToBase64String(mStream.GetBuffer(), 0, (int)mStream.Length);
+
+        }
+        public string decrypt (string encryptedString)
+        {
+            if (String.IsNullOrEmpty(encryptedString))
+            {
+                throw new ArgumentNullException("The string for decryption cannot be empty or null!");
+            }
+            SymmetricAlgorithm saProvider = DES.Create();
+            MemoryStream memStream = new MemoryStream(Convert.FromBase64String(encryptedString));
+            CryptoStream cStream = new CryptoStream(memStream, saProvider.CreateDecryptor(seed,seed), CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cStream);
+            return reader.ReadLine();
+        }
+    }
+}
---
+++ WcfProject1/Service1.svc.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfProject1
+{
+    public class Service1 : IService1
+    {
+        public int C2f(int c)
+        {
+            int f = (c * 9 / 5) + 32;
+            return f;
+        }
+
+        public int F2c(int f)
+        {
+            int c = (f - 32) * 5 / 9;
+            return c;
+        }
+
+        public string sortNums(string nums)
+        {
+            string[] numStrings = nums.Split(',');
+            List<int> numbers = new List<int>();
+
+            foreach (string numString in numStrings)
+            {
+                if (int.TryParse(numString, out int num))
+                {
+                    numbers.Add(num);
+                }
+            }
+
+            numbers.Sort();
+            string[] sortedNumStrings = numbers.Select(n => n.ToString()).ToArray();
+            string sortedResult = string.Join(",", sortedNumStrings);
+
+            return sortedResult;
+        }
+
+        public string encrypt(string text)
+        {
+            WcfProject1.Cryption encrypt = new WcfProject1.Cryption();
+            return encrypt.encrypt(text);
+        }
+
+        public string decrypt(string text)
+        {
+            WcfProject1.Cryption decrypt = new WcfProject1.Cryption();
+            return decrypt.decrypt(text);
+        }
+    }
+}
